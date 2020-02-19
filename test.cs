285d316e07333d05007d5c69a61dd74ae48a0a3a
Dimiter Vakovsky <vakovsky@gmail.com>
2020-02-19 08:58:07 +0200

    Update test.cs

--- test.cs
+++ test.cs
@@ -1,1 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
 
