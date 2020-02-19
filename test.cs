e6a4b2e5e502d3d8af21bcf680191564ed6db8f2
Dimiter Vakovsky <vakovsky@gmail.com>
2020-02-19 15:22:06 +0200

    Update test.cs

--- test.cs
+++ test.cs
@@ -1,16 +1,69 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
-namespace ConsoleApplication1
+namespace HelloWorldApplication
 {
-    class Program
+
+    class Array2d
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello C#");
+
+            int[] b;
+            b = new int[8];
+            int[] c;
+            c = new int[4];
+
+            int[,] a;
+            a = new int[8, 4];
+            //Console.Write("{0} ", a.Length);
+            //Console.Write("{0} ", a.GetLength(0));
+            //Console.Write("{0} ", a.GetLength(1));
+            Random r = new Random();
+
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                for (int j = 0; j < a.GetLength(1); j++)
+                {
+                    a[i, j] = r.Next(-20,50);
+                }
+            }
+
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                for (int j = 0; j < a.GetLength(1); j++)
+                {
+                    Console.Write("{0}   ", a[i,j]);
+                }
+                Console.WriteLine();
+            }
+
+            int max = a[0,0];
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                for (int j = 0; j < a.GetLength(1); j++)
+                {
+                    if (max < a[i, j])
+                    {
+                        max = a[i, j];
+                    }
+                }
+            }
+            Console.WriteLine(max);
+
+            for (int i = 0; i < b.Length; i++)
+            {
+                Console.Write("{0}, ", b[i]);
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < c.Length; i++)
+            {
+                Console.Write("{0}, ", c[i]);
+            }
+            Console.WriteLine();
+
+            Console.ReadKey();
         }
     }
 }
-
