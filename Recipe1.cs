45ef5f645d3345d0c26c9b8e6f41064ea7f49135
Dimiter Vakovsky <vakovsky@gmail.com>
2021-03-29 18:17:23 +0300

    Create Recipe1.cs
    new

---
+++ Recipe1.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void PrintNumbers()
+        {
+            Console.WriteLine("Starting...");
+            for (int i = 1; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(PrintNumbers);
+            t.Start();
+            PrintNumbers();
+        }
+    }
+}
